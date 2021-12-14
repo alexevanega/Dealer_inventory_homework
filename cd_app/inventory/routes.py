b1392e201a9deb123739bc16ab1157efd0255811
alexevanega <alexevanega@gmail.com>
2021-12-14 18:09:32 -0500

    added an api request for chuck norris jokes so that you get a random joke each time you add to your inventory

--- cd_app/inventory/routes.py
+++ cd_app/inventory/routes.py
@@ -9,12 +9,21 @@ inv  = Blueprint('inventory', __name__, template_folder='inv_templates')
 
 from cd_app.models import db
 
+import requests as r
+
 @inv.route('/inventory')
 def inventory_page():
     inventory = Inventory.query.all()
     if inventory == None:
         return render_template('add_to.html')
-    return render_template('data_list.html', title='Inventory Listing')
+    return render_template('data_list.html', inventory=inventory, title='Inventory Listing')
+
+@inv.route('/inventory/chucknorris')
+def chuckJokes():
+    api= r.get('https://api.chucknorris.io/jokes/random')
+    the_jokes = api.json()
+    jokes = the_jokes['value']
+    return render_template('chuckjokes.html', jokes=jokes)
 
 @inv.route('/inventory/add_to_inventory', methods=['GET','POST'])
 def addInv():
@@ -33,5 +42,5 @@ def addInv():
             db.session.add(item)
             db.session.commit()
 
-            return redirect(url_for('homePage'))
+            return redirect(url_for('inventory.chuckJokes'))
     return render_template('add_to.html',form=add_form, title='Add To Inventory')
